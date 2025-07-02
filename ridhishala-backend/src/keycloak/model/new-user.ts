088ac28af4542963429526d598ea474b03e970d9
sanya <modisanya2@gmail.com>
2025-07-02 16:03:09 +0530

    basic authentication for user

---
+++ ridhishala-backend/src/keycloak/model/new-user.ts
@@ -0,0 +1,6 @@
+export class NewUser {
+    username: string;
+    firstName: string;
+    lastName: string;
+    password : string;
+}
---
+++ ridhishala-backend/src/keycloak/model/update-user.ts
@@ -0,0 +1,4 @@
+export class UpdateUser {
+    firstName: string;
+    lastName: string;
+}
