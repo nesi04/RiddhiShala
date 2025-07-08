93dce3114065bff567e00f6255302534b9e1bf47
nesi04 <neeraj579singh@gmail.com>
2025-07-08 18:40:21 +0530

    Small change in mfa file

--- app/activity/mfa/page.tsx
+++ app/activity/mfa/page.tsx
@@ -10,7 +10,7 @@ export default function page(){
         <>
         <div className="flex flex-col items-center justify-center md:pt-20 md:gap-4">
             <h1 className="font-semibold text-4xl ">Multi Factor Authentication Setup</h1>
-            <p>Secure your account by setting up multi factor authentication</p>
+            <p>Secure your account by setting up the multi factor authentication</p>
             <button onClick={handleSubmit} className="w-60 py-2 bg-black rounded-lg text-white">Verify</button>
         </div>
         </>
