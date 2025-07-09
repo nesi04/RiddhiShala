81c99c4d4b746ce0c8fe12c6160ea2940aaec88b
sunny.html <sunnyhaokip04@gmail.com>
2025-07-09 23:17:14 +0530

    Forgot

---
+++ app/(auth)/forgot/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Eye, EyeOff } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+export default function ForgotpasswordPage() {
+  
+        
+    
+  return (
+    <div className="">forgot password</div>
+  );
+}
